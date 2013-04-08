38b48ec3c30e986943c18ff4ec62eca2a860e3b7
Kieran Briggs <kieran.briggs@sheffcol.ac.uk>
2013-04-08 15:14:53 +0100

    update of version to include date formatting
    update of version to include date formatting
    
    Signed-off-by: Kieran Briggs <kieran.briggs@sheffcol.ac.uk>

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
-$plugin->version   = 2013040500;    // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013040802;    // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;    // Requires this Moodle version
 $plugin->component = 'report_departments';  // Full name of the plugin (used for diagnostics)
